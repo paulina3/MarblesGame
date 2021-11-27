d44258821097a9f8556b14a379441a78fef8e9cf
Paulina Cebulska <paulinacebulska.pc@gmail.com>
2021-11-27 23:47:20 +0100

    Added basic logic

--- Marbles.py
+++ Marbles.py
@@ -1,8 +1,8 @@
 import sys
+from random import randint
+
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 from win32api import GetSystemMetrics
-
-# Global variables
 
 # Number of fields in board
 board_size = 9
@@ -10,18 +10,24 @@ board_size = 9
 # Store board information
 board = []
 
-# Colors definitions (RGB)
-gray = "159, 159, 159"
-white = "255, 255, 255"
-blue = "133, 191, 244"
-green = "132, 213, 136"
+# Balls information
+balls_number = 5  # total number of used balls colors (5 - 9)
+next_balls_number = 3  # increase each turn
+next_colors = []  # random next balls colors
+chosen_ball = None  # ball chosen to move
+chosen_destination = None  # destination of chosen ball
+
+# Colors definitions (images)
+white = "white.png"     # blank
+blue = "blue.png"       # number 0
+green = "green.png"     # number 1
+red = "red.png"         # number 2
+yellow = "yellow.png"   # number 3
+purple = "purple.png"   # number 4
+colors_definition = [blue, green, red, yellow, purple, white]
 
 # User's total score
 total_score = 0
-
-# Variables used to move balls
-chosen_ball = 0
-chosen_destination = 0
 
 
 class MainWindow(QMainWindow):
@@ -38,7 +44,6 @@ class MainWindow(QMainWindow):
 
 class Button(QPushButton):
     def __init__(self, pos_x, pos_y, value, main_window):
-
         window_width = main_window.frameGeometry().width()
         window_height = main_window.frameGeometry().height()
 
@@ -51,15 +56,95 @@ class Button(QPushButton):
         self.value = value
 
         self.setFixedSize(button_size, button_size)
-        # self.move(int(window_width / 3 + pos_x + button_size), int(button_size + pos_y + button_size))
         self.move(int(window_width / 4 + button_size * pos_x), int(button_size + button_size * pos_y))
         self.setText(str(self.value))
-        self.setStyleSheet("background-color:rgb({})".format(gray))
+        self.setStyleSheet("border-image : url({})".format(colors_definition[-1]))
         self.show()
-        self.clicked.connect(lambda: self.change_value())
+        self.clicked.connect(lambda: self.move_balls())
 
-    def change_value(self):
-        self.setStyleSheet("background-color:rgb({})".format(green))
+    def change_value(self, color_number):
+        self.value = color_number
+        if color_number is not None:
+            self.setStyleSheet("border-image : url({})".format(colors_definition[color_number]))
+        else:
+            self.setStyleSheet("border-image : url({})".format(colors_definition[-1]))
+        self.setText(str(self.value))
+
+    def move_balls(self):
+        global chosen_ball
+        global chosen_destination
+        if self.value is not None:
+            chosen_ball = board[self.pos_x][self.pos_y]
+            print("Chosen ball position: {}, {}".format(self.pos_x, self.pos_y))
+        elif chosen_ball is not None:
+            if ball_can_be_moved(chosen_ball.pos_x, chosen_ball.pos_y, self.pos_x, self.pos_y):
+                chosen_destination = board[self.pos_x][self.pos_y]
+                print("Chosen destination position: {}, {}".format(self.pos_x, self.pos_y))
+            # else do nothing
+        # else do nothing
+
+        if chosen_ball is not None and chosen_destination is not None:
+            print("Moving color number {}".format(chosen_ball.value))
+            chosen_destination.change_value(chosen_ball.value)
+            chosen_ball.change_value(None)
+
+            # Check for 4 or more in the row
+            check_for_complete()
+
+            # Add new balls to board
+            next_rand_balls()
+
+
+# Checks if ball can be moved to chosen destination
+def ball_can_be_moved(pos_x, pos_y, dest_x, dest_y):
+    can_be_moved = True
+    return can_be_moved
+
+
+# Returns 3 random ball numbers and its positions
+# returns: [[pos_x, pos_y, color],[], ...]
+def next_rand_colors():
+    global next_colors
+    next_colors = []
+
+    # Random colors of next balls
+    for i in range(next_balls_number):
+        next_colors.append(randint(0, balls_number - 1))
+
+
+# Returns 3 random ball numbers and its positions
+# returns: [[pos_x, pos_y, color],[], ...]
+def next_rand_balls():
+    print("Random next balls")
+    global chosen_ball
+    global chosen_destination
+
+    # Reset saved positions
+    chosen_destination = None
+    chosen_ball = None
+    next_balls = []
+
+    # Random balls position
+    for i in range(next_balls_number):
+        pos_x = randint(0, board_size - 1)
+        pos_y = randint(0, board_size - 1)
+        if board[pos_x][pos_y].value != 0:
+            # If filed not empty, try next one
+            pos_x = (pos_x + 1) % board_size
+            pos_y = (pos_y + 1) % board_size
+
+        next_balls.append([pos_x, pos_y, next_colors[i]])
+
+    # Update board
+    for i in range(next_balls_number):
+        pos_x = next_balls[i][0]
+        pos_y = next_balls[i][1]
+        color = next_balls[i][2]
+        board[pos_x][pos_y].change_value(color)
+
+    # Random colors of next balls
+    next_rand_colors()
+    return next_balls
 
 
 # Loads initial board state
@@ -68,8 +153,15 @@ def init_board():
     for i in range(board_size):
         board_row = []
         for j in range(board_size):
-            board_row.append(Button(i, j, 0, window))
+            board_row.append(Button(i, j, None, window))
         board.append(board_row)
+    next_rand_colors()
+    init_balls = next_rand_balls()
+    for i in range(next_balls_number):
+        pos_x = init_balls[i][0]
+        pos_y = init_balls[i][1]
+        color = init_balls[i][2]
+        board[pos_x][pos_y].change_value(color)
 
 
 # Define menu items
@@ -83,8 +175,12 @@ def init_menu(main_window):
     # window.addDockWidget(label)
 
 
-if __name__ == "__main__":
+# Checks if balls can be removed
+def check_for_complete():
+    return 0
 
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
 
